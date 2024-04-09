63a8d60e1333c642b986fda69174105ad55c474f
Alexey Grishin <grishinas696@gmail.com>
2024-04-09 19:13:12 +0300

    modify file urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,5 @@
+URL_MAIN_PAGE = "https://stellarburgers.nomoreparties.site/"
 URL_REGISTRATION = 'https://stellarburgers.nomoreparties.site/register'
+URL_FORGOT_PASSWORD = 'https://stellarburgers.nomoreparties.site/forgot-password'
+URL_LOGIN = "https://stellarburgers.nomoreparties.site/login"
+URL_PROFILE = "https://stellarburgers.nomoreparties.site/account/profile"
