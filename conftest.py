0b07a6d134f9e63a0ef84899add6c6406605dab7
Alexey Grishin <grishinas696@gmail.com>
2024-04-10 17:15:41 +0300

    fix reviews remark. Deleted return and add close instance in driver fixture

--- conftest.py
+++ conftest.py
@@ -6,38 +6,32 @@ from user_data import DataUser
 
 
 @pytest.fixture(scope='function')
-def driver_registration():
-    chrome_driver = webdriver.Chrome()
-    chrome_driver.get(urls.URL_REGISTRATION)
-
-    return chrome_driver
-
-
-@pytest.fixture(scope='function')
-def driver_login():
-    chrome_driver = webdriver.Chrome()
-    chrome_driver.get(urls.URL_LOGIN)
-
-    return chrome_driver
-
-
-@pytest.fixture(scope='function')
 def driver():
     chrome_driver = webdriver.Chrome()
 
-    return chrome_driver
+    yield chrome_driver
+
+    chrome_driver.quit()
 
 
 @pytest.fixture(scope='function')
-def login(driver_login):
-    driver_login.get(urls.URL_LOGIN)
-    user_name = driver_login.find_element(*StellarBurgersLocators.EMAIL)
+def driver_registration(driver):
+    driver.get(urls.URL_REGISTRATION)
+
+
+@pytest.fixture(scope='function')
+def driver_login(driver):
+    driver.get(urls.URL_LOGIN)
+
+
+@pytest.fixture(scope='function')
+def login(driver_login, driver):
+    user_name = driver.find_element(*StellarBurgersLocators.EMAIL)
     user_name.send_keys(DataUser.AUTH_LOGIN)
 
-    user_name = driver_login.find_element(*StellarBurgersLocators.PASSWORD)
+    user_name = driver.find_element(*StellarBurgersLocators.PASSWORD)
     user_name.send_keys(DataUser.AUTH_PASSWORD)
 
-    enter_button = driver_login.find_element(*StellarBurgersLocators.ENTER_BUTTON)
+    enter_button = driver.find_element(*StellarBurgersLocators.ENTER_BUTTON)
     enter_button.click()
 
-    return driver_login
